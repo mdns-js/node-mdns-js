5fecc56e27e04ccff2b6f4fd63a622ec14ca5afd
James Elías Sigurðarson <james@verdandi.is>
2014-07-29 22:30:32 +0000

    Added full protocol support with proper record parsing

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var config = require('./package.json');
-module.exports = require('./lib');
+var st = require('./lib/service_type');
+
+
+module.exports.Mdns = require('./lib');
 module.exports.version = config.version;
 module.exports.name = config.name;
+
+module.exports.ServiceType = st.ServiceType;
+module.exports.makeServiceType = st.makeServiceType;
+module.exports.tcp = st.protocolHelper('tcp');
+module.exports.udp = st.protocolHelper('udp');
 
