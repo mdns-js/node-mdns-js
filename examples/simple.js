07b9e8d7de26d42420f699f36b431ccb28029414
Carlos Guerrero <guerrerocarlos@gmail.com>
2014-07-30 17:58:46 -0400

    Adding origin ip address as default

--- examples/simple.js
+++ examples/simple.js
@@ -2,15 +2,15 @@ var mdns = require('../');
 
 // var browser = new mdns.Mdns(mdns.tcp("googlecast"));
 // console.log(mdns.ServiceType.wildcard);
-var browser = new mdns.Mdns(mdns.ServiceType.wildcard);
+var browser = new mdns.Mdns(mdns.tcp('airplay'));
 
 browser.on('ready', function () {
   browser.discover(); 
 });
 
 browser.on('update', function (data) {
-    console.log('device address', data.addresses[0]); 
+    console.log('device address', data.addresses); 
     console.log('device name', data.name);
     console.log('service name', data.type);
-    mdns.shutdown();
+    //mdns.shutdown();
 });
