6c6df4e0159a4285a5e1a1e61899bad916b7db9c
Peter Magnusson <peter@birchroad.net>
2014-07-31 17:51:49 +0200

    test and comments
    Tests now work with the new API syntax introduced in #4

--- test/mdns.test.js
+++ test/mdns.test.js
@@ -1,70 +1,46 @@
 /*global describe: true, it: true, before: true, after: true */
 //var debug = require('debug')('mdns:test');
 var should = require('should');
-var Mdns = require('../');
+var mdns = require('../');
 
 
 
 describe('mDNS', function () {
-    var mdns;
+    var browser;
     before(function (done) {
-        should.exist(Mdns);
-        mdns = Mdns();
-        mdns.should.be.instanceof(Mdns);
-        mdns.on('ready', function (socketcount) {
+        should.exist(mdns, 'library does not exist!?');
+        browser = mdns.createBrowser();
+
+
+        browser.on('ready', function (socketcount) {
             socketcount.should.be.above(0);
             done();
         });
     });
 
     after(function () {
-        mdns.shutdown();
+        browser.stop();
     });
 
 
     it('should .discover()', function (done) {   
-        mdns.once('update', function () {
+        browser.once('update', function (data) {
             //mdns._byService.should.have.property('_workstation._tcp');
-            
-            var hosts = mdns.ips('_workstation._tcp');
-            hosts.should.be.instanceof(Array);
-            hosts.length.should.be.above(0);
-
-            var services = mdns.services();
-            services.should.be.instanceof(Array);
-            services.length.should.be.above(0);
+            data.should.have.property('interfaceIndex');
+            data.should.have.property('networkInterface');
+            data.should.have.property('addresses');
+            data.should.have.property('query');
+            data.should.have.property('type');
+        
             done();
         });
-        setTimeout(mdns.discover.bind(mdns),500);
+        setTimeout(browser.discover.bind(browser),500);
     });
 
     
-    it('should not crash on missing service when listing ips', function (done) {
-        var missing = mdns.ips('this service should not exist');
-        missing.should.be.instanceof(Array);
-        missing.length.should.equal(0);
-        done();
-    });    
-
-
-    it('should not crash on missing host when listing services', function (done) {
-        var missing = mdns.services('this ip should not exist');
-        missing.should.be.instanceof(Array);
-        missing.length.should.equal(0);
-        done();
-    });
-
     it('should close unused', function (done) {
-        mdns.closeUnused();
+        browser.closeUnused();
         setTimeout(done, 500);
-    });
-
-    describe.skip('advertisements', function () {
-        it('createAdvertisement', function (done) {
-            var ad = mdns.createAdvertisement(Mdns.tcp('http'), 4242);
-            should.exist(ad);
-            done();
-        });
     });
     
 });
