91bf88c3cc55c0eae2eab21296f299759fd9ddf7
Peter Magnusson <peter@birchroad.net>
2014-11-14 22:26:00 +0100

    Testing of advertisement.
    Had to break out some methods to be able to test them.

---
+++ test/advertisement.test.js
@@ -0,0 +1,52 @@
+/*global describe: true, it: true */
+//var debug = require('debug')('mdns:test');
+var should = require('should');
+var pf = require('../lib/packetfactory');
+var mdns = require('../');
+var DNSRecord = require('../lib/dnsrecord');
+
+function mockAdvertisement() {
+  var context = {};
+  context.options = {
+    name: 'hello',
+  };
+  context.nameSuffix = '';
+  context.port = 4242;
+  context.serviceType = mdns.tcp('_http');
+  return context;
+}
+
+describe('packetfactory', function () {
+
+  it('buildQDPacket', function (done) {
+    var context = mockAdvertisement();
+    var packet = pf.buildQDPacket.apply(context, []);
+    context.alias.should.equal('hello._http._tcp.local');
+    should.exist(packet);
+    done();
+  });
+
+
+  it('buildANPacket', function (done) {
+    var context = mockAdvertisement();
+    var packet = pf.buildQDPacket.apply(context, []);
+    pf.buildANPacket.apply(context, [DNSRecord.TTL]);
+    should.exist(packet);
+    done();
+  });
+
+
+  it('createAdvertisement', function () {
+    var service = mdns.createAdvertisement(mdns.tcp('_http'), 9876,
+    {
+      name:'hello',
+      txt:{
+        txtvers:'1'
+      }
+    });
+
+    service.should.have.property('port', 9876);
+    service.should.have.property('options');
+    service.options.should.have.property('name', 'hello');
+  });
+});
